68fb472eadaf6eb81c389ce9e717493b8a2d97e5
jaruizra <121313957+jaruizra@users.noreply.github.com>
2022-12-24 12:21:45 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,4 +1,3 @@
-
 class tests:
     def __init__(self, test):
         d = test
@@ -17,63 +16,57 @@ class tests:
         self.m_score = d_s[2 - 1]["score"]
 
     def harware_description(self):
-        print("--- cpu data ---")
+        print("--- device hardware ---")
         print("CPU name =", self.cpu)
         print("CPU cores =", self.cores)
         print("GPU name =", self.gpu)
         print("Device RAM =", self.ram)
-        print("----------------")
+        print("-----------------------")
 
     def harware_description2(self, d):
-        print("--- cpu data ---")
+        print("--- devices hardware ---")
         print("CPU 1 name =", self.cpu, " --- CPU 2 =", d.cpu)
         print("CPU 1 cores =", self.cores, " --- CPU 2 =", d.cores)
         print("GPU 1 name =", self.gpu, " --- GPU 2 =", d.gpu)
         print("Device 1 RAM =", self.ram, " --- Device 2 =", d.ram)
-        print("----------------")
+        print("------------------------")
 
     def main_c_data(self):
-        print("--- cpu data ---")
-        print("test frequencies =", self.frequency)
-        print("max freq", max(self.frequency))
-        print("min freq", min(self.frequency))
-        print("average freq", sum(self.frequency)/len(self.frequency))
-        print("----------------")
+        print("--- cpu performance ---")
+        print("Max freq =", max(self.frequency))
+        print("Min freq =", min(self.frequency))
+        print("Average freq =", sum(self.frequency)/len(self.frequency))
+        print("Test frequencies =", self.frequency)
+        print("-----------------------")
 
     def main_c_data2(self, d):
-        print("--- cpu data ---")
-        print("Device 1 frequencies =", self.frequency)
-        print("Device 2 frequencies =", d.frequency)
+        print("--- cpu performance ---")
+        print("Average 1 freq =", sum(self.frequency)/len(self.frequency), " --- Average 2 =", sum(d.frequency)/len(d.frequency))
+        print("Max 1 freq =", max(self.frequency), " --- Max 2 =", max(d.frequency))
+        print("Min 1 freq =", min(self.frequency), " --- Min 2 =", min(d.frequency))
+        print("Test 1 frequencies =", self.frequency)
+        print("Test 2 frequencies =", d.frequency)
 
-        print("max 1 freq", max(self.frequency))
-        print("max 2 freq", max(d.frequency))
-
-        print("min 1 freq", min(self.frequency))
-        print("min 2 freq", min(d.frequency))
-
-        print("average 1 freq", sum(self.frequency)/len(self.frequency))
-        print("average 2 freq", sum(d.frequency)/len(d.frequency))
-
-        print("----------------")
+        print("-----------------------")
 
     def results(self):
-        print("--- results ---")
+        print("--- Device Scores ---")
         print("Device single core =", self.s_score)
         print("Device multi core =", self.m_score)
-        print("----------------")
+        print("---------------------")
 
     def results2(self, d2):
-        print("--- results ---")
-        print("Device 1 single core =", self.s_score, " --- Device 2 =", self.s_score)
-        print("Device 1 multi core =", self.m_score, " --- Device 2 =  ", d2.m_score)
-        print("----------------")
+        print("--- Devices Scores ---")
+        print("Test 1 single core =", self.s_score, " --- Test 2 =", self.s_score)
+        print("Test 1 multi core =", self.m_score, " --- Test 2 =  ", d2.m_score)
+        print("----------------------")
 
     def print_compare(self, d2):
         print("")
         print("-----------------------------------------------------------------------")
         print("")
-        print("Model ID 1=", self.model, "   Date =", d2.date)
-        print("Model ID 2=", d2.model, "   Date =", d2.date)
+        print("Device test 1 =", self.model, "   Date =", d2.date)
+        print("Device test 2 =", d2.model, "   Date =", d2.date)
         print("")
         print(" --- BENCHMARK RESULTS --- ")
         self.results2(d2)
@@ -89,7 +82,7 @@ class tests:
         print("")
         print("-----------------------------------------------------------------------")
         print("")
-        print("Model ID =", self.model, "   Date =", self.date)
+        print("Device name =", self.model, "   Date =", self.date)
         print("")
         print(" --- BENCHMARK RESULTS --- ")
         self.results()
